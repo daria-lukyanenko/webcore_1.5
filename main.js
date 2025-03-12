76457e0e7fca5966585951af17968c8fd443d2fe
Daria <ldi@192.168.1.4>
2025-03-12 09:56:54 +0300

    Update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const buttonShow = document.getElementById("btn-show-brands");
-const buttonHide = document.getElementById("btn-hide-brands");
+const buttonToggle = document.getElementById("btn-toggle-brands");
 const tabletBrandsItems = document.querySelectorAll(
   ".brands__item:nth-last-child(-n+5)"
 );
@@ -7,38 +6,39 @@ const desktopBrandsItems = document.querySelectorAll(
   ".brands__item:nth-last-child(-n+3)"
 );
 
-buttonHide.style.display = "none";
+let isBrandsVisible = false;
 
-const showAllBrands = () => {
-  if (window.innerWidth >= 1366) {
-    for (let i = 0; i < desktopBrandsItems.length; i++) {
-      desktopBrandsItems[i].style.display = "flex";
+const toggleBrands = () => {
+  if (isBrandsVisible) {
+    if (window.innerWidth >= 1366) {
+      for (let i = 0; i < desktopBrandsItems.length; i++) {
+        desktopBrandsItems[i].style.display = "none";
+      }
+    } else if (window.innerWidth >= 768) {
+      for (let i = 0; i < tabletBrandsItems.length; i++) {
+        tabletBrandsItems[i].style.display = "none";
+      }
     }
-    buttonShow.style.display = "none";
-    buttonHide.style.display = "flex";
-  } else if (window.innerWidth >= 768) {
-    for (let i = 0; i < tabletBrandsItems.length; i++) {
-      tabletBrandsItems[i].style.display = "flex";
+    buttonToggle.querySelector(".button__text").textContent = "Показать все";
+    buttonToggle
+      .querySelector(".button__icon")
+      .classList.remove("button__icon-rotate");
+  } else {
+    if (window.innerWidth >= 1366) {
+      for (let i = 0; i < desktopBrandsItems.length; i++) {
+        desktopBrandsItems[i].style.display = "flex";
+      }
+    } else if (window.innerWidth >= 768) {
+      for (let i = 0; i < tabletBrandsItems.length; i++) {
+        tabletBrandsItems[i].style.display = "flex";
+      }
     }
-    buttonShow.style.display = "none";
-    buttonHide.style.display = "flex";
+    buttonToggle.querySelector(".button__text").textContent = "Скрыть";
+    buttonToggle
+      .querySelector(".button__icon")
+      .classList.add("button__icon-rotate");
   }
+  isBrandsVisible = !isBrandsVisible;
 };
 
-buttonShow.addEventListener("click", showAllBrands);
-
-const hideBrands = () => {
-  if (window.innerWidth >= 1366) {
-    for (let i = 0; i < desktopBrandsItems.length; i++) {
-      desktopBrandsItems[i].style.display = "none";
-    }
-  } else if (window.innerWidth >= 768) {
-    for (let i = 0; i < tabletBrandsItems.length; i++) {
-      tabletBrandsItems[i].style.display = "none";
-    }
-  }
-  buttonShow.style.display = "flex";
-  buttonHide.style.display = "none";
-};
-
-buttonHide.addEventListener("click", hideBrands);
+buttonToggle.addEventListener("click", toggleBrands);
